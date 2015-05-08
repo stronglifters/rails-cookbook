f3e2c3447d64cd97b388f36c6d576601b623a11b
mo khan <mo@mokhan.ca>
2015-05-07 22:16:30 -0600

    add a rails recipe to tie it all together.

---
+++ recipes/rails.rb
@@ -0,0 +1,25 @@
+include_recipe "mokhan-myface::rbenv"
+include_recipe "mokhan-myface::capistrano"
+include_recipe "mokhan-myface::nginx"
+include_recipe "mokhan-myface::unicorn"
+include_recipe "mokhan-myface::nodejs"
+
+[
+  'imagemagick',
+  'libmagickwand-dev',
+  'memcached',
+  'logrotate',
+  'sqlite3',
+].each do |item|
+  package item do
+    action :install
+  end
+end
+
+shared_path = "#{node['capistrano']['root_path']}/shared"
+
+template "/etc/logrotate.d/rails" do
+  source "rails_logrotate.erb"
+  mode "0644"
+  variables({ shared_path: shared_path })
+end
