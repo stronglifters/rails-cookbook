d98a0ca592364c8f843cf056af1ac439b421c76d
mo khan <mo@mokhan.ca>
2015-05-22 16:01:33 -0600

    allow new user to manage their home directory.

--- recipes/user.rb
+++ recipes/user.rb
@@ -1,8 +1,8 @@
 configuration = node['user']
 
 user configuration['username'] do
+  supports manage_home: true
   comment "rails user"
   home "/home/#{configuration['username']}"
   shell "/bin/bash"
 end
-
