cb4b3a5df9461facacd3e38e48374c65accdfbf3
mo khan <mo@mokhan.ca>
2015-05-23 22:23:50 -0600

    specify permissions for capistrano folder.

--- recipes/capistrano.rb
+++ recipes/capistrano.rb
@@ -11,8 +11,9 @@ directories = [
   "#{shared_path}/bundle",
   "#{shared_path}/config",
   "#{shared_path}/log",
-  "#{shared_path}/pids",
-  "#{shared_path}/sockets",
+  "#{shared_path}/tmp/sockets",
+  "#{shared_path}/tmp/pids",
+  "#{shared_path}/tmp/cache",
   "#{root_path}/releases",
 ]
 
@@ -32,6 +33,13 @@ template "#{shared_path}/.env.#{node.chef_environment}" do
   group configuration['username']
   mode "0600"
   variables(configuration)
+end
+
+execute "chown-rails" do
+  command "chown -R rails:rails #{root_path}"
+  user "root"
+  action :run
+  not_if "stat -c %U #{root_path} | grep root"
 end
 
 template "#{shared_path}/config/database.yml" do
