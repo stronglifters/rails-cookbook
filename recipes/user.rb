948586298f0991d652ecd2dede61e1bfb7b3aed1
mo khan <mo@mokhan.ca>
2015-11-11 13:35:25 -0700

    fix broken unit tests.

--- recipes/user.rb
+++ recipes/user.rb
@@ -1,6 +1,6 @@
-user node['stronglifters']['username'] do
+user node["stronglifters"]["username"] do
   supports manage_home: true
   comment "rails user"
-  home "/home/#{node['stronglifters']['username']}"
+  home "/home/#{node["stronglifters"]["username"]}"
   shell "/bin/bash"
 end
